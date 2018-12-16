24981eb1159f7c58f69b91b7264bbef54080d97c
Jeremy Lincoln <bossco28@gmail.com>
2018-12-16 09:59:04 -0500

    Added in future for the Quack api and included import for api, http, json and datetime

--- lib/quake_info.dart
+++ lib/quake_info.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert'; //convert json to strings
+import 'package:intl/intl.dart' as intl;
 
 class Quake extends StatelessWidget {
   @override
@@ -13,4 +17,14 @@ class Quake extends StatelessWidget {
       body: Center(),
     );
   }
+}
+
+//future type object
+Future<List> getJson() async {
+  String apiURL =
+      'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson';
+
+  http.Response response = await http.get(apiURL);
+
+  return json.decode(response.body); //this returns a List type by default
 }
