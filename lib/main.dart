600e03bdf36adf48e3761cef24a52cdc7bb0f90d
Jeremy Lincoln <bossco28@gmail.com>
2018-12-17 06:07:12 -0500

    All one file for now.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,23 @@
 import 'package:flutter/material.dart';
-import 'quake_info.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert'; //convert json to strings
+import 'package:intl/intl.dart' as intl;
 
 void main() async {
-  runApp(MaterialApp(title: "Quake App", home: Quake()));
+  runApp(MaterialApp(
+    title: "Quake App",
+    debugShowCheckedModeBanner: false,
+    home: Scaffold(),
+  ));
+}
+
+//future type object
+Future<List> getQuackJson() async {
+  String apiURL =
+      'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson';
+
+  http.Response response = await http.get(apiURL);
+
+  return json.decode(response.body); //this returns a List type by default
 }
