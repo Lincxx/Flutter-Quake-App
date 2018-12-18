8b4990d0b2c3d2598321609132e81a21f76bedf0
Jeremy Lincoln <bossco28@gmail.com>
2018-12-18 07:03:06 -0500

    Displaying Datetime for current activity

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,96 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 import 'dart:async';
 import 'dart:convert'; //convert json to strings
-import 'package:intl/intl.dart' as intl;
+import 'package:http/http.dart' as http;
+import 'package:intl/intl.dart';
 
 void main() async {
+  Map _data = await getQuackJson();
+  var _featureList = _data['features'];
+
+  print(_featureList.length);
+  print(_featureList[0]['properties']['place']);
+
+  String _returnDateTime(int position) {
+    DateTime _date = new DateTime.fromMillisecondsSinceEpoch(
+        _featureList[position]['properties']['time']);
+    var _formatter = new DateFormat("yMMMMd").add_jm();
+    var _dateString = _formatter.format(_date);
+    return _dateString;
+  }
+
   runApp(MaterialApp(
-    title: "Quake App",
-    debugShowCheckedModeBanner: false,
-    home: Scaffold(),
+    title: "Intro to Parse",
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('JSON Parse'),
+        centerTitle: true,
+        backgroundColor: Colors.orangeAccent,
+      ),
+      body: Center(
+        child: ListView.builder(
+          itemCount: _featureList.length,
+          padding: EdgeInsets.all(14.5),
+          itemBuilder: (BuildContext context, int position) {
+            return Column(
+              children: <Widget>[
+                Divider(height: 5.5),
+                ListTile(
+                  title: Text(
+                    "${_returnDateTime(position)}",
+                    style: TextStyle(fontSize: 17.9),
+                  ),
+                  subtitle: Text(
+                    "${_featureList[position]['properties']['place']}",
+                    style: TextStyle(
+                        fontSize: 13.9,
+                        color: Colors.grey,
+                        fontStyle: FontStyle.italic),
+                  ),
+                  leading: CircleAvatar(
+                      backgroundColor: Colors.greenAccent,
+                      child: Text(
+                          "${_featureList[position]['properties']['mag']}"
+                              .toUpperCase(),
+                          style: TextStyle(
+                              fontSize: 16.4,
+                              fontWeight: FontWeight.w400,
+                              color: Colors.white))),
+                  onTap: () => _showOnTapMessage(
+                      context, _featureList[position]['properties']['place']),
+                ),
+              ],
+            );
+          },
+        ),
+      ),
+    ),
   ));
 }
 
+//whenever you are building a new view or widget you must pass the context
+void _showOnTapMessage(BuildContext context, String message) {
+  var alert = AlertDialog(
+    title: Text("Quake"),
+    content: Text(message),
+    actions: <Widget>[
+      FlatButton(
+          child: Text(
+            "Ok",
+            style: TextStyle(color: Colors.white),
+          ),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          color: Colors.blueAccent)
+    ],
+  );
+  //showDialog(context: context, child: alert);
+  showDialog(context: context, builder: (context) => alert);
+}
+
 //future type object
-Future<List> getQuackJson() async {
+Future<Map> getQuackJson() async {
   String apiURL =
       'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson';
 
